481bd89216a44d28642966f38c1c7d7201980786
ajib6ept <asalbert@narod.ru>
2022-08-07 13:39:53 +0300

    refactoring tests

--- tests/test_download_url.py
+++ tests/test_download_url.py
@@ -4,7 +4,6 @@ import tempfile
 import requests_mock
 from page_loader.engine import download
 
-from tests.fixtures.html_response import original_reponse, changed_html
 
 TEST_URL = "https://ru.hexlet.io/courses"
 TEST_URL_FILE_NAME = "ru-hexlet-io-courses.html"
@@ -16,9 +15,15 @@ TEST_FULL_URL_PNG_FILE = "https://ru.hexlet.io/assets/professions/nodejs.png"
 TEST_FULL_URL_JS_FILE = "https://ru.hexlet.io/packs/js/runtime.js"
 TEST_FULL_URL_CSS_FILE = "https://ru.hexlet.io/assets/application.css"
 
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
+ORIGINAL_RESPONSE_PATH = os.path.join(TEST_DATA_DIR, "original_response.txt")
+CHANGED_RESPONSE_PATH = os.path.join(TEST_DATA_DIR, "changed_html.txt")
+
 
 def test_page_loader():
     with requests_mock.Mocker() as m, tempfile.TemporaryDirectory() as tmpdirname:  # noqa: E501
+        original_reponse = open(ORIGINAL_RESPONSE_PATH).read()
+        changed_html = open(CHANGED_RESPONSE_PATH).read()
         m.get(TEST_URL, text=original_reponse)
         m.get(TEST_FULL_URL_PNG_FILE, text="1")
         m.get(TEST_FULL_URL_JS_FILE, text="1")
@@ -30,6 +35,8 @@ def test_page_loader():
 
 def test_page_loader_change_original_html():
     with requests_mock.Mocker() as m, tempfile.TemporaryDirectory() as tmpdirname:  # noqa: E501
+        original_reponse = open(ORIGINAL_RESPONSE_PATH).read()
+        changed_html = open(CHANGED_RESPONSE_PATH).read()
         m.get(TEST_URL, text=original_reponse)
         m.get(TEST_FULL_URL_PNG_FILE, text="1")
         m.get(TEST_FULL_URL_JS_FILE, text="1")
@@ -41,6 +48,7 @@ def test_page_loader_change_original_html():
 
 def test_page_loader_download_files():
     with requests_mock.Mocker() as m, tempfile.TemporaryDirectory() as tmpdirname:  # noqa: E501
+        original_reponse = open(ORIGINAL_RESPONSE_PATH).read()
         m.get(TEST_URL, text=original_reponse)
         m.get(TEST_FULL_URL_PNG_FILE, text="1")
         m.get(TEST_FULL_URL_JS_FILE, text="1")

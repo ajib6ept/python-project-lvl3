fbf591be770df0ef0a943f00c7f2272cd67ff8a9
ajib6ept <asalbert@narod.ru>
2022-08-14 15:56:42 +0300

    refactoring

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -19,65 +19,69 @@ TEXT_FILE_EXTENSION = (".css", ".js", ".html")
 def download(url, save_path, loglevel="INFO"):
     create_logger(loglevel)
     logging.debug(f"Start with {url} to save in {save_path}")
-    url_values = prepare_url(url, save_path)
-    html_doc = download_page(url)
-    soup = BeautifulSoup(html_doc, "html.parser")
-    all_html_resources = []
-    for tag, attr in HTML_RESOURCES.items():
-        new_soup, resources = change_html(tag, attr, soup, url_values)
-        all_html_resources.extend(resources)
-    all_html_resources = remove_double_from_the_list(all_html_resources)
-    logging.debug(
-        f"Found {len(all_html_resources)} unique resources to download"
-    )
-    mk_dir(url_values["url_save_dir_name"])
-    download_resources(all_html_resources)
-    html_doc_new = new_soup.prettify()
-    save_page(html_doc_new, url_values["url_save_path"])
-    url_save_path = url_values["url_save_path"]
-    logging.info(f"Page was successfully downloaded into '{url_save_path}'")
-    return url_values["url_save_path"]
 
-
-def prepare_url(url, save_path):
-    url_parse_result = urlparse(url)
-    url_domain_changed = url_parse_result.netloc.replace(".", "-")
-    url_path_changed = url_parse_result.path.replace("/", "-")
+    # create the necessary variables
+    url_domain_changed = urlparse(url).netloc.replace(".", "-")
+    url_path_changed = urlparse(url).path.replace("/", "-")
     if url_path_changed == "-":
         url_path_changed = ""
     url_file_name = url_domain_changed + url_path_changed
     url_save_path = os.path.join(save_path, url_file_name + ".html")
     url_save_dir_name = os.path.join(save_path, f"{url_file_name}_files")
-    return {
-        "url_domain_changed": url_domain_changed,
-        "url_file_name": url_file_name,
-        "url_save_path": url_save_path,
-        "url": url,
-        "url_save_dir_name": url_save_dir_name,
-    }
+
+    html_doc = download_page(url)
+    soup = BeautifulSoup(html_doc, "html.parser")
+    all_html_resources = []
+    for tag, attr in HTML_RESOURCES.items():
+        new_soup, resources = change_html(
+            tag,
+            attr,
+            soup,
+            url,
+            url_save_dir_name,
+            url_domain_changed,
+            url_file_name,
+        )
+        all_html_resources.extend(resources)
+    all_html_resources = remove_double_from_the_list(all_html_resources)
+    logging.debug(
+        f"Found {len(all_html_resources)} unique resources to download"
+    )
+    mk_dir(url_save_dir_name)
+    download_resources(all_html_resources)
+    html_doc_new = new_soup.prettify()
+    save_page(html_doc_new, url_save_path)
+    logging.info(f"Page was successfully downloaded into '{url_save_path}'")
+    return url_save_path
 
 
-def change_html(tag, attr, soup, url_values):
+def change_html(
+    tag,
+    attr,
+    soup,
+    url,
+    url_save_dir_name,
+    url_domain_changed,
+    url_file_name,
+):
     resources = []
     for el in soup.find_all(tag):
         el_src = el.get(attr)
-        if el_src is not None and is_same_domain(el_src, url_values["url"]):
+        if el_src is not None and is_same_domain(el_src, url):
             new_el_src = str(urlparse(el_src).path)
             new_el_src = new_el_src.replace("/", "-").replace("'", "")
-            new_el_src = url_values["url_domain_changed"] + new_el_src
+            new_el_src = url_domain_changed + new_el_src
             if "." not in new_el_src:
                 new_el_src = f"{new_el_src}.html"
-            new_el_src_with_dir = (
-                f"{url_values['url_save_dir_name']}/{new_el_src}"
-            )
+            new_el_src_with_dir = f"{url_save_dir_name}/{new_el_src}"
             resources.append(
                 {
                     "file_url": el_src,
                     "save_file_path": new_el_src_with_dir,
-                    "page_url": url_values["url"],
+                    "page_url": url,
                 }
             )
-            el[attr] = f"{url_values['url_file_name']}_files/{new_el_src}"
+            el[attr] = f"{url_file_name}_files/{new_el_src}"
     return soup, resources
 
 
